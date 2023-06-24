eabb77a2fe703eb3c0a21fa414ab8cd8fafbfd02
Ben Lynch <bdlynch7@gmail.com>
2023-06-23 23:16:52 -0500

    #6 and started stretch

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -36,14 +36,27 @@ console.log(supplyChanges);
 console.log('6. Showing supplyChanges...');
 for (let x = 0; x < supplyChanges.length; x++) {
     if(supplyChanges[x] > 0) {
-        console.log('Added', [x], 'parts');
-    }
-}
+        console.log('Added', supplyChanges[x], 'parts.');
+    } else if(supplyChanges === 0) {
+        console.log('No Change.');
+    } else if (supplyChanges < 0) {
+        console.log('Removed', supplyChanges[x], 'parts.');
+    } // end if 
+} // end for
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (x of supplyChanges) {
+    if (supplyChanges[x] > 0) {
+        console.log('Added', x, 'parts.');
+    } else if (x === 0) {
+        console.log('No change.');
+    } else if (x < 0) {
+        console.log('Removed', x, 'parts.')
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
